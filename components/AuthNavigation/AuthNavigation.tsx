ee622451d00a3f29fedd9da9676b206d4138c1ae
Jenya Linx <sportpit2020@gmail.com>
2026-07-06 23:39:07 +0300

    feat: redesign responsive neon header

--- components/AuthNavigation/AuthNavigation.tsx
+++ components/AuthNavigation/AuthNavigation.tsx
@@ -1,30 +1,41 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
 import css from './AuthNavigation.module.css';
 import { useAuthStore } from '@/lib/store/authStore';
 import { logout } from '@/lib/api/clientApi';
-import { useRouter } from 'next/navigation';
 
-export default function AuthNavigation() {
+type AuthNavigationProps = {
+  onNavigate?: () => void;
+};
+
+export default function AuthNavigation({ onNavigate }: AuthNavigationProps) {
   const { isAuthenticated, user } = useAuthStore();
   const clear = useAuthStore((s) => s.clearIsAuthenticated);
   const router = useRouter();
+  const pathname = usePathname();
 
   const handleLogout = async () => {
     await logout();
     clear();
+    onNavigate?.();
     router.push('/sign-in');
   };
 
   if (isAuthenticated) {
+    const isProfileActive = pathname.startsWith('/profile');
+
     return (
       <>
         <li className={css.navigationItem}>
           <Link
             href="/profile"
             prefetch={false}
-            className={css.navigationLink}
+            className={`${css.navigationLink} ${
+              isProfileActive ? css.active : ''
+            }`}
+            onClick={onNavigate}
           >
             Profile
           </Link>
@@ -32,12 +43,11 @@ export default function AuthNavigation() {
 
         <li className={css.navigationItem}>
           <div className={css.userContainer}>
-            <p className={css.userEmail}>{user?.email}</p>
+            <p className={css.userEmail} title={user?.email}>
+              {user?.email}
+            </p>
 
-            <button
-              onClick={handleLogout}
-              className={css.logoutButton}
-            >
+            <button onClick={handleLogout} className={css.logoutButton}>
               Logout
             </button>
           </div>
@@ -46,13 +56,19 @@ export default function AuthNavigation() {
     );
   }
 
+  const isSignInActive = pathname.startsWith('/sign-in');
+  const isSignUpActive = pathname.startsWith('/sign-up');
+
   return (
     <>
       <li className={css.navigationItem}>
         <Link
           href="/sign-in"
           prefetch={false}
-          className={css.navigationLink}
+          className={`${css.navigationLink} ${
+            isSignInActive ? css.active : ''
+          }`}
+          onClick={onNavigate}
         >
           Login
         </Link>
@@ -62,7 +78,10 @@ export default function AuthNavigation() {
         <Link
           href="/sign-up"
           prefetch={false}
-          className={css.navigationLink}
+          className={`${css.navigationLink} ${
+            isSignUpActive ? css.active : ''
+          }`}
+          onClick={onNavigate}
         >
           Sign up
         </Link>
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,24 +1,60 @@
-import Link from "next/link";
-import css from "./Header.module.css";
-import AuthNavigation from "../AuthNavigation/AuthNavigation";
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
+import css from './Header.module.css';
+import AuthNavigation from '../AuthNavigation/AuthNavigation';
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const pathname = usePathname();
+
+  const closeMenu = () => setIsMenuOpen(false);
+
+  const isHomeActive = pathname === '/';
+  const isNotesActive = pathname.startsWith('/notes');
+
   return (
     <header className={css.header}>
       <nav className={css.nav}>
-        <ul className={css.navigationList}>
+        <Link href="/" className={css.logo} onClick={closeMenu}>
+          Note Flow
+        </Link>
+
+        <button
+          type="button"
+          className={`${css.burger} ${isMenuOpen ? css.burgerActive : ''}`}
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+          aria-label="Toggle navigation menu"
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+
+        <ul className={`${css.navigationList} ${isMenuOpen ? css.menuOpen : ''}`}>
           <li className={css.navigationItem}>
-            <Link href="/" className={css.link}>
+            <Link
+              href="/"
+              className={`${css.link} ${isHomeActive ? css.active : ''}`}
+              onClick={closeMenu}
+            >
               Home
             </Link>
           </li>
 
           <li className={css.navigationItem}>
-            <Link href="/notes/filter/all" className={css.link}>
+            <Link
+              href="/notes/filter/all"
+              className={`${css.link} ${isNotesActive ? css.active : ''}`}
+              onClick={closeMenu}
+            >
               Notes
             </Link>
           </li>
-          <AuthNavigation />
+
+          <AuthNavigation onNavigate={closeMenu} />
         </ul>
       </nav>
     </header>
