09f76596e6e3945a71fd20a05c794ac106aff227
Jenya Linx <sportpit2020@gmail.com>
2026-07-08 01:36:53 +0300

    fix: merge remote patterns and public api url

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+
   images: {
     remotePatterns: [
       {
@@ -8,6 +9,10 @@ const nextConfig: NextConfig = {
         hostname: 'ac.goit.global',
       },
     ],
+  },
+
+  env: {
+    NEXT_PUBLIC_API_URL: 'https://notehub-api.goit.study',
   },
 };
 
